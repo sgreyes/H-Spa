5ab4bf477ce93b485ff69150e93a930715e8f47a
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-10-23 09:10:07 +0800

    added new customer view

--- Prototype/RecInt/views/customer/index.php
+++ Prototype/RecInt/views/customer/index.php
@@ -1,9 +1,36 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>customer/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\CustomerSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Customers';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="customer-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Customer', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'cus_fname',
+            'cus_lname',
+            'cus_contact_number',
+            'cus_email:email',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
