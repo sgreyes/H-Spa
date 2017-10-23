a2c5b702b647c16bc3ddd302af52905e1278bfe2
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-10-23 09:13:58 +0800

    created new rooms crud

--- Prototype/RecInt/views/rooms/index.php
+++ Prototype/RecInt/views/rooms/index.php
@@ -1,9 +1,33 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>rooms/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\RoomsSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Rooms';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="rooms-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Rooms', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'room_num',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
