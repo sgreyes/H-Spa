213a0e14a1b3543eab7d154a8c36b535117dc7b2
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-10-23 09:11:27 +0800

    added new services controller

---
+++ Prototype/RecInt/controllers/ServicesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class ServicesController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
