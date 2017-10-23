b056e3642ec704ecf669e4a94dbff7465b2f3d3b
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-10-23 09:12:39 +0800

    new rooms model

---
+++ Prototype/RecInt/models/Rooms.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "rooms".
+ *
+ * @property integer $id
+ * @property integer $room_num
+ *
+ * @property ServiceDetails[] $serviceDetails
+ */
+class Rooms extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'rooms';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['room_num'], 'required'],
+            [['room_num'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'room_num' => 'Room Num',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getServiceDetails()
+    {
+        return $this->hasMany(ServiceDetails::className(), ['rooms_id' => 'id']);
+    }
+}
