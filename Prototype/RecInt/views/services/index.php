29b5e7b3a294e5790e011627e5a9def7dc5dee06
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-10-23 09:12:07 +0800

    new services crud

--- Prototype/RecInt/views/services/index.php
+++ Prototype/RecInt/views/services/index.php
@@ -1,9 +1,35 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>services/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\ServicesSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Services';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="services-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Services', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'ID',
+            'srvc_name',
+            'srvc_duration',
+            'srvc_price',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
