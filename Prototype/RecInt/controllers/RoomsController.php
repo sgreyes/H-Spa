e05307ec27cca414dabda1e58e56e05e8302ba50
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-10-23 09:13:10 +0800

    created new rooms controller

---
+++ Prototype/RecInt/controllers/RoomsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class RoomsController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
